193f390a2d239c51d575ea9ff7269b86d678c9db
Manzar-E-Hassin <mnz247@hotmail.com>
2025-03-13 18:54:43 +0600

    Update softRTC.h

--- softRTC.h
+++ softRTC.h
@@ -33,65 +33,55 @@ Published at 13 March, 2025 @ 5:22 PM (UTC+6)
 #include "Arduino.h"
 #include <stdint.h>
 
-class softRTC
+class softRTC 
 {
   public:
-    // Constructor 
-   //softRTC() {}
     // Methods
     void write(uint8_t day, uint8_t month, uint16_t year, uint8_t hour, uint8_t minute, uint8_t second, bool isPM, bool is12H);
-    void read(uint8_t &day, uint8_t &month, uint16_t &year, uint8_t &hour, uint8_t &minute, uint8_t &second, bool & isPM, bool &is12H, uint8_t &week);
+    void read(uint8_t &day, uint8_t &month, uint16_t &year, uint8_t &hour, uint8_t &minute, uint8_t &second, bool &isPM, bool &is12H, uint8_t &week);
     void print();
-    String Weekdays(uint8_t week)  __attribute__((always_inline));
-    inline bool syncStatus() {return (clkset.sync_);}
-    String leadingZero(uint8_t value) __attribute__((always_inline));
+    inline bool syncStatus() { return clkset.sync_; }
     
+    // Mode definitions
     #define AM 0
     #define PM 1
     #define MODE_12H 1
     #define MODE_24H 0
 
   private:
-
-    bool isLeapYear(uint16_t year) __attribute__((always_inline));
-    inline void manageYear(bool select, uint16_t& Full, uint8_t& Half, bool century);
-    inline void Convert_To_12h(uint8_t &hour, bool &isPM) __attribute__((always_inline));
-    inline void Convert_To_24h(uint8_t &hour, bool isPM) __attribute__((always_inline));
-    inline uint8_t getWeekdays(uint8_t day, uint8_t month, uint16_t year) __attribute__((always_inline));
-    inline void errorMsg(uint8_t val) __attribute__((always_inline));
-    inline bool syncStatus_() __attribute__((always_inline));
-    void setCenturybit(uint16_t year) __attribute__((always_inline));
+    bool isLeapYear(uint16_t year);
+    void manageYear(bool select, uint16_t &Full, uint8_t &Half, bool century);
+    void Convert_To_12h(uint8_t &hour, bool &isPM);
+    void Convert_To_24h(uint8_t &hour, bool isPM);
+    uint8_t getWeekdays(uint8_t day, uint8_t month, uint16_t year);
+    void errorMsg(uint8_t val);
+    bool syncStatus_();
+    void setCenturybit(uint16_t year);
     void calcTime(uint16_t &year, uint8_t &month, uint8_t &day, uint8_t &hour, uint8_t &minute, uint8_t &second);
-    void print(uint16_t year, uint8_t month, uint8_t day,uint8_t hour, uint8_t minute, uint8_t second, bool is12H);
+    void print(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second, bool is12H);
 
-    // Starting date and time
-    struct  __attribute__((packed)) rtcSettings
-    {
-      bool   sync_   : 1; // Sync Status
-      bool    is12H  : 1; // 12h time
-      uint32_t millis; // Store the millis() value when the simulation starts
+    // Starting date/time settings (packed to save RAM)
+    struct __attribute__((packed)) rtcSettings {
+      bool sync_ : 1;      // Sync status
+      bool is12H : 1;      // 12-hour mode flag
+      uint32_t millis;     // Stored millis() value when set
     } clkset;
 
-    #define rtc_bit_ampm    1
-    #define rtc_bit_12h     2
-    #define rtc_bit_century 3
     #define rtc_short_year  0
     #define rtc_full_year   1
     #define error_invalid_date  0
     #define error_not_sync      1
 
   protected:
-
-    struct  __attribute__((packed)) clocksyncTime
-      {
-        uint8_t day    : 5; // 1-31
-        uint8_t month  : 4; // 1-12
-        uint8_t year   : 7; // 00-99 Short Year
-        uint8_t hour   : 5; // 00-23
-        uint8_t minute : 6; // 00-59
-        uint8_t second : 6; // 00-59
-        uint8_t century: 1; // century bit 0= 2000, 1= 2100
-      } startclk;
-
-};          // end of class
-#endif      // end of ifndef soft_rtc_h
+    // Packed time values (minimizes RAM usage)
+    struct __attribute__((packed)) clocksyncTime {
+      uint8_t day    : 5; // 1-31
+      uint8_t month  : 4; // 1-12
+      uint8_t year   : 7; // 00-99 (short year)
+      uint8_t hour   : 5; // 00-23
+      uint8_t minute : 6; // 00-59
+      uint8_t second : 6; // 00-59
+      uint8_t century: 1; // 0 = 2000, 1 = 2100
+    } startclk;
+};
+#endif // soft_rtc_h
