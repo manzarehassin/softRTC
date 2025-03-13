24d5ba309c184120114c4b88d42ce38f05a8ff2f
Manzar-E-Hassin <mnz247@hotmail.com>
2025-03-13 17:25:25 +0600

    Update softRTC.h

--- softRTC.h
+++ softRTC.h
@@ -24,77 +24,53 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-Published at 8 Dec, 2024 @ 9:05 PM (UTC+6)
+Published at 13 March, 2025 @ 5:22 PM (UTC+6)
 
 */
-
 
 #ifndef soft_rtc_h
 #define soft_rtc_h
 #include "Arduino.h"
+#include <stdint.h>
 
 class softRTC
 {
   public:
     // Constructor 
-   softRTC() {flush();}
+   //softRTC() {}
     // Methods
-    inline void flush() {memset(&clk,0,sizeof(clk));clk.year=24; clk.week=2;clk.day=clk.month=1;}
-    inline void begin() {clk.enable_=1;}
-    inline void stop()  {clk.enable_=0; flush();}
+    void write(uint8_t day, uint8_t month, uint16_t year, uint8_t hour, uint8_t minute, uint8_t second, bool isPM, bool is12H);
+    void read(uint8_t &day, uint8_t &month, uint16_t &year, uint8_t &hour, uint8_t &minute, uint8_t &second, bool & isPM, bool &is12H, uint8_t &week);
+    void print();
+    String Weekdays(uint8_t week)  __attribute__((always_inline));
+    inline bool syncStatus() {return (clkset.sync_);}
+    String leadingZero(uint8_t value) __attribute__((always_inline));
     
-    void write(uint8_t day, uint8_t month, uint16_t year, uint8_t hour, uint8_t minute, uint8_t second, bool ampm, bool mode12h);
-    void read(uint8_t &day, uint8_t &month, uint16_t &year, uint8_t &hour, uint8_t &minute, uint8_t &second, bool & ampm, bool &mode12h, uint8_t &week);
-    void print() __attribute__((always_inline));
-    void update() __attribute__((always_inline));
-    
-    inline uint8_t hour()   {return clk.hour;}
-    inline uint8_t minute() {return clk.minute;}
-    inline uint8_t second() {return clk.second;}
-    inline bool is24hrs()   {return !clk.mode12;}
-    inline bool isPM()      {return clk.ampm;}
-    inline String ampm()    {return clk.ampm ? F("PM") : F("AM");}
-    inline uint8_t day()    {return clk.day;}
-    inline uint8_t month()  {return clk.month;}
-    inline uint16_t year()  {return (20 + clk.century) * 100 + clk.year;}  
-    inline String week()    {return getNames(clk.week);}
-    String leadingZero(uint8_t value);
-    inline bool syncStatus() {return (clk.enable_ and clk.sync_);}
-
     #define AM 0
     #define PM 1
     #define MODE_12H 1
     #define MODE_24H 0
 
   private:
-  
-    inline bool checkLeapYear(uint16_t& year) __attribute__((always_inline));
-    inline void manageYear(bool select, uint16_t& Full, uint8_t& Half, bool century) __attribute__((always_inline));
-    inline uint8_t getDays(uint16_t& year, uint8_t month) __attribute__((always_inline));
-    inline void Convert_To_12h(uint8_t& _Hour, bool& _AMPM) __attribute__((always_inline));
-    inline void Convert_To_24h(uint8_t& _Hour, bool & _AMPM) __attribute__((always_inline));
-    inline uint8_t getWeekdays(uint8_t day, uint8_t month, uint16_t& year) __attribute__((always_inline));
-    inline void hourValidator(uint8_t& hr, bool rw) __attribute__((always_inline)); // rw: 0= read, 1= write
-    inline void yearValidator(uint16_t& year, bool rw) __attribute__((always_inline)); // rw: 0= read, 1= write
+
+    bool isLeapYear(uint16_t year) __attribute__((always_inline));
+    inline void manageYear(bool select, uint16_t& Full, uint8_t& Half, bool century);
+    inline void Convert_To_12h(uint8_t &hour, bool &isPM) __attribute__((always_inline));
+    inline void Convert_To_24h(uint8_t &hour, bool isPM) __attribute__((always_inline));
+    inline uint8_t getWeekdays(uint8_t day, uint8_t month, uint16_t year) __attribute__((always_inline));
     inline void errorMsg(uint8_t val) __attribute__((always_inline));
     inline bool syncStatus_() __attribute__((always_inline));
-    String getNames(uint8_t val) __attribute__((always_inline));
+    void setCenturybit(uint16_t year) __attribute__((always_inline));
+    void calcTime(uint16_t &year, uint8_t &month, uint8_t &day, uint8_t &hour, uint8_t &minute, uint8_t &second);
+    void print(uint16_t year, uint8_t month, uint8_t day,uint8_t hour, uint8_t minute, uint8_t second, bool is12H);
 
-    struct clockStorage
-      {
-        uint8_t day    : 5; // 1-31
-        uint8_t month  : 4; // 1-12
-        uint8_t year   : 7; // 00-99 Short Year
-        uint8_t hour   : 5; // 00-23
-        uint8_t minute : 6; // 00-59
-        uint8_t second : 6; // 00-59
-        uint8_t week   : 3; // 1-7
-        uint8_t ampm   : 1; // 0= am, 1= pm
-        uint8_t mode12 : 1; // 0 = 24hr, 1 = 12h
-        uint8_t century: 1; // century bit 0= 2000, 1= 2100
-        bool   enable_ : 1; // enable rtc
-        bool   sync_   : 1; // Sync Status
-      }clk;
+    // Starting date and time
+    struct  __attribute__((packed)) rtcSettings
+    {
+      bool   sync_   : 1; // Sync Status
+      bool    is12H  : 1; // 12h time
+      uint32_t millis; // Store the millis() value when the simulation starts
+    } clkset;
 
     #define rtc_bit_ampm    1
     #define rtc_bit_12h     2
@@ -103,5 +79,19 @@ class softRTC
     #define rtc_full_year   1
     #define error_invalid_date  0
     #define error_not_sync      1
+
+  protected:
+
+    struct  __attribute__((packed)) clocksyncTime
+      {
+        uint8_t day    : 5; // 1-31
+        uint8_t month  : 4; // 1-12
+        uint8_t year   : 7; // 00-99 Short Year
+        uint8_t hour   : 5; // 00-23
+        uint8_t minute : 6; // 00-59
+        uint8_t second : 6; // 00-59
+        uint8_t century: 1; // century bit 0= 2000, 1= 2100
+      } startclk;
+
 };          // end of class
 #endif      // end of ifndef soft_rtc_h
